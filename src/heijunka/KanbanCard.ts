5cec21aa0a928439ac030926e212aa901908791b
Axel <axelkr.dev@google.com>
2021-01-23 06:09:12 +0100

    store project id as well

--- src/heijunka/KanbanCard.ts
+++ src/heijunka/KanbanCard.ts
@@ -3,13 +3,18 @@ import { Property } from './Property';
 export class KanbanCard {
     readonly name: Property<string>;
     readonly id: string;
+    readonly project: string;
 
-    constructor(id: string, name: string, createdAt: Date) {
+    constructor(id: string, name: string, createdAt: Date, project: string) {
         if (typeof id === "undefined") {
             throw new Error('id cannot be undefined');
         }
+        if (typeof project === "undefined") {
+            throw new Error('project cannot be undefined');
+        }
         this.name = new Property<string>(name, createdAt);
         this.id = id;
+        this.project = project;
     }
 
     rename(newName: string, updatedAt: Date) : KanbanCard {
@@ -17,6 +22,6 @@ export class KanbanCard {
         if (updatedNameProperty.value === this.name.value) {
             return this;
         }
-        return new KanbanCard(this.id,newName,updatedAt);
+        return new KanbanCard(this.id,newName,updatedAt, this.project);
     }
 }
--- test/heijunka/KanbandCard.spec.ts
+++ test/heijunka/KanbandCard.spec.ts
@@ -4,24 +4,30 @@ import { KanbanCard } from '../../src/heijunka/KanbanCard';
 
 describe('KanbanCard', () => {
   it('constructor expects that id is defined', () => {
-    expect(function(){new KanbanCard(undefined,'aString',new Date())}).throws();
+    expect(function(){new KanbanCard(undefined,'aString',new Date(),'aProjectId')}).throws();
   });
 
   it('constructor expects that value is defined', () => {
-    expect(function(){new KanbanCard('aId',undefined,new Date())}).throws();
+    expect(function(){new KanbanCard('aId',undefined,new Date(),'aProjectId')}).throws();
   });
 
   it('constructor expects that Date is defined', () => {
-    expect(function(){new KanbanCard('aId','aString',undefined)}).throws();
+    expect(function(){new KanbanCard('aId','aString',undefined,'aProjectId')}).throws();
+  });
+
+  it('constructor expects that project is defined', () => {
+    expect(function(){new KanbanCard('aId','aString',new Date(),undefined)}).throws();
   });
 
   it('constructor values are stored', () => {
     const aValue = 'aString';
     const aId = 'aId';
+    const aProjectId = 'aProjectId';
     const aDate = new Date();
-    const aKanbanCard = new KanbanCard(aId,aValue, aDate);
+    const aKanbanCard = new KanbanCard(aId,aValue, aDate,aProjectId);
     expect(aKanbanCard.name.value).to.equal(aValue);
     expect(aKanbanCard.id).to.equal(aId);
+    expect(aKanbanCard.project).to.equal(aProjectId);
   });
 
   it('rename keeps current value if updated happened before', () => {
@@ -30,7 +36,7 @@ describe('KanbanCard', () => {
     const anotherValue = 'anotherString';
     const initialDate = new Date(2020,12,24);
     const beforeInitialDate = new Date(2019,11,23);
-    const aKanbanCard = new KanbanCard(id,initialValue,initialDate);
+    const aKanbanCard = new KanbanCard(id,initialValue,initialDate,'aProjectId');
     const renamedKanbanCard = aKanbanCard.rename(anotherValue,beforeInitialDate);
     expect(renamedKanbanCard.name.value).to.equal(initialValue);
   });
@@ -41,7 +47,7 @@ describe('KanbanCard', () => {
     const anotherValue = 'anotherString';
     const initialDate = new Date(2020,12,24);
     const afterInitialDate = new Date(2021,12,25);
-    const aKanbanCard = new KanbanCard(id,initialValue,initialDate);
+    const aKanbanCard = new KanbanCard(id,initialValue,initialDate,'aProjectId');
     const renamedKanbanCard = aKanbanCard.rename(anotherValue,afterInitialDate);
     expect(renamedKanbanCard.name.value).to.equal(anotherValue);
   });
