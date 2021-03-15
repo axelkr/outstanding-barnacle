8310df24611a1f1607ff0432089eec747513900e
Axel <axelkr.dev@google.com>
2021-03-16 00:10:25 +0100

    expose command

--- src/objectEvents/objectEventFactory.ts
+++ src/objectEvents/objectEventFactory.ts
@@ -2,6 +2,7 @@ import { StateModel } from '../heijunka/StateModel';
 import { UUIDGenerator } from '../heijunka/UUIDGenerator';
 
 import { CreateStateModelCommand } from './CreateStateModelCommand';
+import { CreateContextCommand } from './CreateContextCommand';
 
 import { ObjectEvent, Topic } from 'choicest-barnacle';
 
@@ -9,5 +10,10 @@ export class ObjectEventFactory {
   public createStateModel(topic: Topic, stateModel: StateModel): ObjectEvent {
     const createStateModelCommand = new CreateStateModelCommand();
     return createStateModelCommand.createEvent(topic, stateModel, UUIDGenerator.createUUID());
+  }
+
+  public createContext(topic: Topic, name: string): ObjectEvent {
+    const createContextCommand = new CreateContextCommand();
+    return createContextCommand.createEvent(topic, name, UUIDGenerator.createUUID());
   }
 }
