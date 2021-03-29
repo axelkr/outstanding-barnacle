84ecbe3d8f39c2b7b470aa5abeaf1fb7a28d9966
axelkr <axelkr.dev@gmail.com>
2021-03-29 20:27:16 +0200

    allow nesting of stuff

--- src/heijunka/TaskCollection.ts
+++ src/heijunka/TaskCollection.ts
@@ -3,13 +3,15 @@ import { IdObjectCollection } from './IdObjectCollection';
 
 export class TaskCollection {
     private tasks: IdObjectCollection<Task>;
+    private children: Map<string,string[]>;
 
-    private constructor(tasks: IdObjectCollection<Task>) {
+    private constructor(tasks: IdObjectCollection<Task>, children: Map<string,string[]>) {
         this.tasks = tasks;
+        this.children = children;
     }
 
     public static createEmptyCollection(): TaskCollection {
-        return new TaskCollection(new IdObjectCollection<Task>([]));
+        return new TaskCollection(new IdObjectCollection<Task>([]), new Map<string,string[]>());
     }
 
     public add(task: Task): TaskCollection {
@@ -18,15 +20,29 @@ export class TaskCollection {
         if (noChange) {
             return this;
         }
-        return new TaskCollection(updatedTasks);
+        const updatedChildren = new Map<string,string[]>(this.children);
+        updatedChildren.set(task.id,[]);
+        if ( !updatedChildren.has(task.parent())) {
+            updatedChildren.set(task.parent(),[]);
+        }
+        updatedChildren.set(task.parent(),[...updatedChildren.get(task.parent()),task.id]);
+        return new TaskCollection(updatedTasks, updatedChildren);
     }
 
     public has(id: string): boolean {
         return this.tasks.has(id);
     }
 
-    public getTasks(): Task[] {
-        return this.tasks.idObjects;
+    public getChildrenOf(id: string): Task[] {
+        if (!this.children.has(id)) {
+            return [];
+        }
+        const childIds: string[] = this.children.get(id);
+        const result: Task[] = [];
+        childIds.forEach(anId => {
+            result.push(this.get(anId));
+        });
+        return result;
     }
 
     public get(id: string): Task {
@@ -44,6 +60,6 @@ export class TaskCollection {
     }
 
     private replace(updatedTask: Task): TaskCollection {
-        return new TaskCollection(this.tasks.replace(updatedTask.id, updatedTask));
+        return new TaskCollection(this.tasks.replace(updatedTask.id, updatedTask), this.children);
     }
 }
