0e606a542c3ae5539f82ecf22c120e873a188969
Axel <axelkr.dev@google.com>
2021-04-10 06:42:22 +0200

    constants in a uuid just waste space at no benefit

--- src/heijunka/UUIDGenerator.ts
+++ src/heijunka/UUIDGenerator.ts
@@ -1,7 +1,7 @@
 export class UUIDGenerator {
   public static createUUID(): string {
-    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) { // eslint-disable-line
-      var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);// eslint-disable-line
+    return 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'.replace(/x/g, function (c) { // eslint-disable-line
+      var r = Math.random() * 16 | 0, v = r;// eslint-disable-line
       return v.toString(16);
     });
   }
